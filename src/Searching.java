10a945a1361db4c20f1910cac67bc8c6d960ab7f
QuantumPineapple <nachiketj14@gmail.com>
2024-10-25 14:14:22 +0530

    DSA 1

--- src/Searching.java
+++ src/Searching.java
@@ -95,4 +95,13 @@ public class Searching {
         }
         return 0;
     }
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
--- src/Sorting.java
+++ src/Sorting.java
@@ -6,22 +6,6 @@ public class Sorting {
         System.out.println((cyclicSort(arr)));
     }
 
-    static int[] binarySearchIn2D(int[][] arr, int target) {
-        int r = 0;
-        int c = arr.length - 1;
-
-        while (r < arr.length && c >= 0) {
-            if (arr[r][c] == target) {
-                return new int[]{r, c};
-            }
-            if (arr[r][c] > target) {
-                c--;
-            } else {
-                r++;
-            }
-        }
-        return new int[]{-1, -1};
-    }
 
     static int[] bubbleSort(int[] arr) {
 
@@ -77,5 +61,34 @@ public class Sorting {
             }
         }
         return nums.length+1;
+    }
+
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // find the max item in the remaining array and swap with correct index
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+    static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (arr[j] < arr[j-1]) {
+                    swap(arr, j, j-1);
+                } else {
+                    break;
+                }
+            }
+        }
     }
 }
