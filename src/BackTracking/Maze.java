7d2426f8aee4e6d5f64a377e46a49257e6647cec
QuantumPineapple <nachiketj14@gmail.com>
2024-12-27 16:35:18 +0530

    maze update Diagonal

--- src/BackTracking/Maze.java
+++ src/BackTracking/Maze.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Maze {
     public static void main(String[] args) {
-        maze("",3,3);
+        System.out.println(mazeRetListDiagonal("",3,3));
     }
 
     static void maze(String p, int a, int b){
@@ -20,5 +20,47 @@ public class Maze {
         if (b>1){
         maze(p+'R',a,b-1);
         }
+    }
+
+    static ArrayList<String> mazeRetList(String p, int a, int b){
+        if (a==1 && b==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if (a>1) {
+            list.addAll(mazeRetList(p + 'D', a - 1, b));
+        }
+        if (b>1){
+            list.addAll(mazeRetList(p+'R',a,b-1));
+        }
+        return list;
+
+    }
+
+    static ArrayList<String> mazeRetListDiagonal(String p, int a, int b){
+        if (a==1 && b==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if (a>1) {
+            list.addAll(mazeRetListDiagonal(p + 'D', a - 1, b));
+        }
+        if (b>1){
+            list.addAll(mazeRetListDiagonal(p+'R',a,b-1));
+        }
+        if (a>1 && b>1){
+            list.addAll(mazeRetListDiagonal(p+'d',a-1,b-1));
+        }
+
+        return list;
+
     }
 }
