cbe1728a3ec5a700d3dda7de82b53b3f2ccf8912
QuantumPineapple <nachiketj14@gmail.com>
2025-01-14 11:29:02 +0530

    leetcode

--- src/Strings/Questions.java
+++ src/Strings/Questions.java
@@ -1,12 +1,24 @@
 package Strings;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Questions {
     public static void main(String[] args) {
-        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println(maxSubArray(arr));
+//        String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+//        List<List<String>> result = groupAnagrams(input);
+//
+//        System.out.println("Grouped Anagrams:");
+//        for (List<String> group : result) {
+//            System.out.print(group);
+//        }
+
+        int[] arr = {1,3,4,5,0,0,0};
+        int[] arr2 = {4,6,7};
+        merge(arr,4,arr2,3);
+
     }
 
     static ArrayList<String> diceRet(String p, int target) {
@@ -54,4 +66,88 @@ public class Questions {
         }
         return maxSum;
     }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            java.util.Arrays.sort(chars);
+            String sortedStr = new String(chars);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static int[] repeatedNumber(final int[] A) {
+        int missing = 0;
+        int repeated =0;
+        Arrays.sort(A);
+        int max = A[0];
+        int min = A[A.length-1];
+
+        for (int i = 0; i < A.length-1; i++) {
+            if (A[i]==A[i+1]){
+                repeated=A[i];
+            }
+        }
+
+        for (int i = min; i < max; i++) {
+            int index = Arrays.binarySearch(A,A[i]);
+            if (index == -1){
+                missing = A[i];
+            }
+        }
+
+        return new int[] {repeated,missing};
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0; // Handle edge case of an empty array
+        }
+
+        int writePtr = 1; // Position for the next unique element
+        for (int readPtr = 1; readPtr < nums.length; readPtr++) {
+            if (nums[readPtr] != nums[writePtr - 1]) { // Compare current with last unique
+                nums[writePtr] = nums[readPtr]; // Update the array in place
+                writePtr++; // Move to the next position for unique elements
+            }
+        }
+
+        return writePtr;
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int num2Count=0;
+        for (int i = m; i < m+n; i++) {
+            if (nums1[i]==0){
+                nums1[i]=nums2[num2Count];
+                num2Count++;
+            }
+        }
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        if (nums.length==0){
+            List<List<Integer>> list = new ArrayList<>();
+            list.addAll();
+            return list;
+    }
+
 }
